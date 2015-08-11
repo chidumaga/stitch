4738a85a473a30bfc8c7a079c823522ceadc0750
Chidumaga <chidumaga@gmail.com>
2015-08-11 09:46:03 +0100

    can only follow another user once

--- app/controllers/follow_controller.rb
+++ app/controllers/follow_controller.rb
@@ -4,10 +4,17 @@ class FollowController < ApplicationController
 		@follow = current_user.follows.build(:followed_user_id => params[:followed_user_id])
 		if @follow.save
 			flash[:notice] = "Followed"
-			redirect_to root_url
+			redirect_to user_path(params[:followed_user_id])
 		else
 			flash[:notice] = "Unable to follow"
-			redirect_to root_url
+			redirect_to user_path(params[:followed_user_id])
 		end
+	end
+	
+	def destroy
+		@follow = current_user.follows.find_by(user_id: params[:id])
+		@follow.destroy
+		flash[:notice] = "Unfollowed"
+		redirect_to user_path(params[:id])
 	end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+	def ids_of_users_following(user)
+		followed_user_id_list = []
+		for followed_user in user.follows
+			followed_user_id_list << followed_user.user_id
+		end #replace loop with an array method such as .select
+		followed_user_id_list
+		end
 end
